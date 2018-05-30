9bdad41957116bf541816e386f86f747adffee4f
小粽子 <862275679@qq.com>
2018-05-30 10:23:46 +0800

    str+arr

--- src/helpers.php
+++ src/helpers.php
@@ -5,3 +5,21 @@
  * Date: 2018/5/29
  * Time: 14:48
  */
+
+if (!function_exists('xRange'))
+{
+    function xRange($n = 1000000)
+    {
+        for ($i = 1; $i < $n; $i++) {
+            yield $i;
+        }
+    }
+}
+
+if (!function_exists('echoS'))
+{
+    function echoS($n = 1)
+    {
+        echo "------------------------ $n ------------------------<br>";
+    }
+}
--- src/index.php
+++ src/index.php
@@ -8,6 +8,9 @@
 
 namespace OMGZui;
 
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use OMGZui\Obj\objFunc;
 use OMGZui\Str\useStr;
 
@@ -17,7 +20,7 @@ require_once __DIR__ . '/bootstrap.php';
 $str = new useStr('  我是1个疯子，有医生开的证明ya  ');
 dump($str->get()->trim());
 
-echo "------------------------ 1 ------------------------<br>";
+echoS(1);
 
 // 魔术方法
 $obj = new objFunc();//__construct __destruct
@@ -34,7 +37,9 @@ isset($obj->foo);// __isset
 unset($obj->foo);// __unset
 $obj_new = clone $obj;// __clone
 dump($obj_new);
-echo "------------------------ 2 ------------------------<br>";
+
+echoS(2);
+
 $start = memory_get_usage();
 $arr = [];
 //$arr = range(1, 1000000);
@@ -47,16 +52,30 @@ $arr = xRange(1000000);
 $end = memory_get_usage();
 echo ($end - $start) / 1000 . "kb<br>";
 
-foreach ($arr as $n => $item) {
-    if ($n < 10)
-    {
-        echo $item;
-    }
-}
+echoS(3);
+$ss = Str::random();
+dump(Str::upper($ss));
+dump(Str::title($ss));
 
-function xRange($n = 1000000)
-{
-    for ($i = 1; $i < $n; $i++) {
-        yield $i;
-    }
-}
+$arr = [
+    [
+        'id' => 90,
+        'age' => 200
+    ],
+    [
+        'id' => 2,
+        'age' => 100
+    ],
+    [
+        'id' => 200,
+        'age' => -100
+    ],
+];
+
+$col = new Collection($arr);
+dump($col);
+dump($col->all() === $arr);
+dump($col->toArray() === $arr);
+dump($col->toJson() === json_encode($arr));
+
+dump(Arr::random($arr));
